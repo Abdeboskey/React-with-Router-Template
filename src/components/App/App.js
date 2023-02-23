e5ae57991531478295c8ee2e18af26e7d0c6e7a7
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2023-02-22 17:17:50 -0700

    Set up config for aliases and basic css vars

---
+++ src/components/App/App.js
@@ -0,0 +1,25 @@
+import logo from 'assets/seed.png';
+import './App.css';
+import Footer from 'components/Footer/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Howdy y'all, I'm
+        </p>
+        <h1>
+          Aaron Burris-DeBoskey
+        </h1>
+        <p>
+          I'm a software engineer, musician, & inline skater from Denver, CO.
+        </p>
+      </header>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
