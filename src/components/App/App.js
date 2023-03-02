2f3c3007fc4687d2ccf4bbb339c014637da549d6
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2023-03-01 18:31:04 -0700

    Create header and navbar

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,12 @@
 import logo from 'assets/seed.png';
 import './App.css';
 import Footer from 'components/Footer/Footer';
+import Header from 'components/Header/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -14,7 +16,8 @@ function App() {
           Aaron Burris-DeBoskey
         </h1>
         <p>
-          ~ a software engineer, musician, & inline skater from Denver, CO.
+          ~ a software engineer, musician, & inline skater
+          <br />from Denver, CO.
         </p>
       </header>
       {/* <Footer /> */}
