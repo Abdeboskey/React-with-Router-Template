b6c58de463e588ce712f932ac4262069c65b9c31
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2023-03-01 19:15:39 -0700

    Create alias for pages directory

--- craco.config.js
+++ craco.config.js
@@ -5,6 +5,7 @@ module.exports = {
         alias: {
             assets: path.resolve(__dirname, 'src/assets/'),
             components: path.resolve(__dirname, 'src/components/'),
+            pages: path.resolve(__dirname, 'src/pages'),
         },
     },
 };
