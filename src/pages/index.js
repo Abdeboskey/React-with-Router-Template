d03b1a0ca427e75f12570dfda629c2681eaa03cf
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2023-03-02 21:47:07 -0700

    Make index files for easier exports

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import Home from 'pages/Home/Home';
+import Software from 'pages/Software/Software';
+import Music from 'pages/Music/Music';
+import Skating from 'pages/Skating/Skating';
+
+export {
+  Home,
+  Software,
+  Music,
+  Skating,
+};
